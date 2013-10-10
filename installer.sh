da18d9d0653a92bdeb9d077cdb3e2c81d370a93d
Iblis Lin <e196819@hotmail.com>
2013-10-10 19:07:58 +0800

    Let '/etc/rc' store in a variable

--- installer.sh
+++ installer.sh
@@ -2,6 +2,10 @@
 
 SSH_DIR=/etc/ssh
 RC_TMP_FILE="/tmp/installer.rc.tmp.$$"
+RC_SCRIPT_FILE='/etc/rc'
+RC_BACKUP_FILE='/etc/rc.orig'
+RC_STARTLINE=`cat -n $RC_SCRIPT_FILE | awk '/rc.subr/ { print $1 }'`
+RC_ENDLINE=`cat -n $RC_SCRIPT_FILE | tail -n 1 | awk '{ print $1 }'`
 BSDINIT_URL="https://github.com/pellaeon/bsd-cloudinit/archive/master.tar.gz"
 
 
@@ -16,11 +20,8 @@ fetch -o - BSDINIT_URL | tar -xzvf - -C '/root'
 rm -vf $SSH_DIR/ssh_host*
 
 touch $RC_TMP_FILE
-RC_STARTLINE=`cat -n /etc/rc | awk '/rc.subr/ { print $1 }'`
-RC_ENDLINE=`cat -n /etc/rc | tail -n 1 | awk '{ print $1 }'`
-RC_BACKUP_FILE='/etc/rc.orig'
-cp -pvf /etc/rc $RC_BACKUP_FILE
-head -n $RC_STARTLINE /etc/rc > $RC_TMP_FILE
-echo "(cd /root/bsd-cloudinit-master/ && $PYTHON ./cloudinit && mv $RC_BACKUP_FILE /etc/rc )" >> $RC_TMP_FILE
-tail -n `expr $RC_ENDLINE -  $RC_STARTLINE` /etc/rc >> $RC_TMP_FILE
-cp -pvf $RC_TMP_FILE /etc/rc
+cp -pvf $RC_SCRIPT_FILE $RC_BACKUP_FILE
+head -n $RC_STARTLINE $RC_SCRIPT_FILE > $RC_TMP_FILE
+echo "(cd /root/bsd-cloudinit-master/ && $PYTHON ./cloudinit && cp -pvf $RC_BACKUP_FILE $RC_SCRIPT_FILE )" >> $RC_TMP_FILE
+tail -n `expr $RC_ENDLINE -  $RC_STARTLINE` $RC_SCRIPT_FILE >> $RC_TMP_FILE
+cp -pvf $RC_TMP_FILE $RC_SCRIPT_FILE
