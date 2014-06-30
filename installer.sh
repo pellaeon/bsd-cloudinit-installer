2598442e14dca3856c873f08d6a3977cb1303d38
Iblis Lin <e196819@hotmail.com>
2014-06-30 17:37:01 +0800

    Fix fetch instruction

--- installer.sh
+++ installer.sh
@@ -9,6 +9,7 @@ BSDINIT_URL="https://github.com/pellaeon/bsd-cloudinit/archive/master.tar.gz"
 
 BSD_VERSION=`uname -r | cut -d. -f 1`
 INSTALL_PKGS='devel/py-setuptools'
+VERIFY_PEER=''
 
 # For FreeBSD10 get root certs and use them
 if [ "$BSD_VERSION" -ge 10 ];then
