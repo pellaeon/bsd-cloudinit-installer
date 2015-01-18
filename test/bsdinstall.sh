ef394786ab0e2d38b8bba9ffaf93213f8b27acf8
Iblis Lin <e196819@hotmail.com>
2015-01-18 19:55:19 +0800

    Add gpt label for root filesystem.

--- test/bsdinstall.sh
+++ test/bsdinstall.sh
@@ -4,7 +4,28 @@
 FETCH='fetch --no-verify-peer'
 INSTALLER='/root/install.sh'
 
+##############################################
+#  utils
+##############################################
+
+cleanup(){
+	rm /etc/resolv.conf
+}
+
+
+##############################################
+#  main block
+##############################################
+
+trap 'cleanup' 0 1 2 15
+
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+# Add gpt label
+gpart modify -i 2 -l rootfs $MD_DEV
+sed -i '' "s/$MD_DEVp2/gpt\/rootfs/" /etc/fstab
+
+# install bsd cloudinit
 
 if [ ! $BSDINIT_INSTALLER_URL ]
 then
