67b6f0a4bcd7c1b2fbef238e6583a74033a4c95c
Iblis Lin <e196819@hotmail.com>
2015-01-19 09:37:58 +0800

    Debug with network

--- test/bsdinstall.sh
+++ test/bsdinstall.sh
@@ -8,10 +8,10 @@ INSTALLER='/root/install.sh'
 #  utils
 ##############################################
 
-cleanup(){
+cleanup(){ #{{{
 	rm -v /etc/resolv.conf
 	exit
-}
+} #}}}
 
 
 ##############################################
@@ -20,11 +20,22 @@ cleanup(){
 
 trap 'cleanup' 0 1 2 15
 
+set -e
+
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
 # Add gpt label
 gpart modify -i 2 -l rootfs $MD_DEV
 sed -i '' "s/${MD_DEV}p2/gpt\/rootfs/" /etc/fstab
+
+echo '/etc/fstab'
+cat /etc/fstab
+echo '================================'
+echo '/etc/resolv.conf'
+cat /etc/resolv.conf
+
+# testing network
+ping -c 5 8.8.8.8
 
 # install bsd cloudinit
 
