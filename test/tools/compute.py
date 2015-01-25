934c4bbbf905c7ae458197244d9936afd9e8c40e
Iblis Lin <e196819@hotmail.com>
2015-01-25 14:37:59 +0800

    using nova rebuild command

--- test/tools/compute.py
+++ test/tools/compute.py
@@ -13,19 +13,19 @@ logging.basicConfig(level=logging.DEBUG)
 
 def boot(image_name=env['OS_IMG_NAME'], flavor_name=env['OS_FLAVOR'],
         net_label=env['OS_NET'], vm_name=env['OS_VM_NAME'], keypair=env['OS_KEYPAIR']):
-    try:
-        i = nova.servers.find(name=vm_name)
-        logging.info('instance {name} already exists. Delete it.'.format(name=vm_name))
-        i.delete()
-    except NotFound as e:
-        pass
-
     image = nova.images.find(name=image_name)
-    flavor = nova.flavors.find(name=flavor_name)
-    net = nova.networks.find(label=net_label)
-    nics = [{'net-id': net.id}]
-    instance = nova.servers.create(name=vm_name, image=image,
-        flavor=flavor, key_name=keypair, nics=nics)
+
+    try:
+        instance = nova.servers.find(name=vm_name)
+        logging.info('instance {name} already exists.'.format(name=vm_name))
+        logging.info('Rebuild it with image<{img}>.'.format(img=image_name))
+        instance.rebuild(image)
+    except NotFound as e:
+        flavor = nova.flavors.find(name=flavor_name)
+        net = nova.networks.find(label=net_label)
+        nics = [{'net-id': net.id}]
+        instance = nova.servers.create(name=vm_name, image=image,
+            flavor=flavor, key_name=keypair, nics=nics)
 
     logging.info('instance name: {}'.format(instance.name))
     logging.info('instance flavor: {}'.format(pformat(instance.flavor)))
